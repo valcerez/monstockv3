f60b8af282ce63aba6b91100942d6cd16aa7e064
valcerez <valentin.cerez@gmail.com>
2025-04-22 18:40:12 +0200

    Init Vite React PWA + Firebase config + PWA icons

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,6 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp }  from "firebase/app";
+import { getFirestore }   from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCCVNDY_joWsabUd9uSDkwghZ4_mxqrYf0",
   authDomain: "monstockv3.firebaseapp.com",
@@ -16,6 +11,5 @@ const firebaseConfig = {
   measurementId: "G-KEVRHVPPSY"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const db = getFirestore(app);
